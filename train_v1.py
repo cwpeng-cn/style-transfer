d446ea0b6bbb88238a65e6eb210e2ccd1bed8f5e
“cwpeng-cn” <cw.peng@foxmail.com>
2021-10-24 10:27:31 +0800

    change optim to LBFGS

--- train_v1.py
+++ train_v1.py
@@ -25,29 +25,33 @@ content_losses = model.content_losses
 input_image = content_image.clone().to(device)
 
 # define optimizer
-optimizer = optim.SGD([input_image.requires_grad_()], lr=0.0001)
+optimizer = optim.LBFGS([input_image.requires_grad_()])
 
 print("Start training......")
+# We use arrays instead of single variables,because use variable will lead a mistake
+# (When passing parameters, the array passes the memory address)
+step = [0]
+while step[0] < 300:
+    def closure():
+        input_image.data.clamp_(0, 1)
+        optimizer.zero_grad()
+        net(input_image)
+        style_score = 0
+        content_score = 0
+        for style_loss in style_losses:
+            style_score = style_score + 100000 * style_loss.loss
+        for content_loss in content_losses:
+            content_score = content_score + content_loss.loss
+        loss = style_score + content_score
+        loss.backward()
+        if step[0] % 10 == 0:
+            print("step:", step[0], " style_loss:", style_score.data, " content_loss:", content_score.data)
+        step[0] += 1
+        return loss
 
-step = 0
-while step < 3000:
-    input_image.data.clamp_(0, 1)
-    optimizer.zero_grad()
-    net(input_image)
-    style_score = 0
-    content_score = 0
-    for style_loss in style_losses:
-        style_score = style_score + 100000 * style_loss.loss
-    for content_loss in content_losses:
-        content_score = content_score + content_loss.loss
-    loss = style_score + content_score
-    loss.backward()
-    if step % 10 == 0:
-        print("step:", step, " style_loss:", style_score.data, " content_loss:", content_score.data)
-    step += 1
-    optimizer.step()
 
-input_img = input_image.cpu().data.clamp_(0, 1)
+    optimizer.step(closure)
+input_img = input_image.cpu().dataclamp_(0, 1)
 show_image(input_img)
 save_image(input_img, CONTENT_IMAGE_PATH.split("/")[-1])
 print('End of the training')
