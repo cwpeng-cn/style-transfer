80189e4625752e75eafcd89a81a0de4f3f8a50b8
“cwpeng-cn” <cw.peng@foxmail.com>
2021-12-11 14:41:51 +0800

    set alpha

--- train.py
+++ train.py
@@ -24,30 +24,32 @@ style_losses = model.style_losses
 content_losses = model.content_losses
 
 # 定义输入
-input_image = torch.randn(content_image.data.size()).cuda()
+input_image = torch.randn(content_image.data.size()).to(device)
 # input_image = content_image.clone().to(device)
 
 # define optimizer
 optimizer = optim.LBFGS([input_image.requires_grad_()])
+alpha = 100000
 
 print("Start training......")
-step = [0]
-while step[0] < 500:
+step = 0
+while step < 500:
     def closure():
+        global step
         input_image.data.clamp_(0, 1)
         optimizer.zero_grad()
         net(input_image)
         style_score = 0
         content_score = 0
         for style_loss in style_losses:
-            style_score = style_score + 100000 * style_loss.loss
+            style_score = style_score + alpha * style_loss.loss
         for content_loss in content_losses:
             content_score = content_score + content_loss.loss
         loss = style_score + content_score
         loss.backward()
-        if step[0] % 10 == 0:
-            print("step:", step[0], " style_loss:", style_score.data, " content_loss:", content_score.data)
-        step[0] += 1
+        if step % 10 == 0:
+            print("step:", step, " style_loss:", style_score.data, " content_loss:", content_score.data)
+        step += 1
         return loss
 
 
